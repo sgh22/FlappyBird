5683e09ced78f57f3c939c6579eb1c55b3bf4975
Stefania Besta <irisanna@Admins-MacBook-Pro-3.local>
2025-04-01 10:57:29 +0000

    Committing unstaged changes/ commit vesen eftir fugla myndir og sound 1.0

--- src/main/java/hi/flappybird/Bird.java
+++ src/main/java/hi/flappybird/Bird.java
@@ -1,4 +1,52 @@
 package hi.flappybird;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Bird {
+
+    private Sprite bird;
+    private ArrayList<Sprite> flight = new ArrayList<>();
+    private int currentBird = 0;
+    private double locationX = 70;
+    private double locationY = 200;
+    private int BIRD_WIDTH = 50;
+    private int BIRD_HEIGHT = 45;
+
+    public Bird() {
+        bird = new Sprite();
+        bird.resizeImage("/images/bird1.png", BIRD_WIDTH, BIRD_HEIGHT);
+        bird.setPositionXY(locationX, locationY);
+        setFlightAnimation();
+        }
+
+        public void setFlightAnimation() {
+            Sprite bird2 = new Sprite();
+            bird2.resizeImage("/images/bird2.png", BIRD_WIDTH, BIRD_HEIGHT);
+            bird2.setPositionXY(locationX, locationY);
+
+            Sprite bird3 = new Sprite();
+            bird3.resizeImage("/images/bird1.png", BIRD_WIDTH, BIRD_HEIGHT);
+            bird3.setPositionXY(locationX, locationY);
+
+            Sprite bird4 = new Sprite();
+            bird4.resizeImage("/images/bird3.png", BIRD_WIDTH, BIRD_HEIGHT);
+            bird4.setPositionXY(locationX, locationY);
+
+            flight.addAll(Arrays.asList(bird, bird2, bird3, bird4));
+        }
+
+        public Sprite getBird() {
+            return bird;
+        }
+
+        public Sprite animate() {
+            if (currentBird == flight.size() - 1) {
+                currentBird = 0;
+            }
+
+            return flight.get(currentBird++);
+        }
+    }
+
 }
--- src/main/java/hi/flappybird/Sound.java
+++ src/main/java/hi/flappybird/Sound.java
@@ -1,4 +1,18 @@
 package hi.flappybird;
 
+import javafx.scene.media.AudioClip;
+
 public class Sound {
+    private AudioClip soundEffect;
+
+    public Sound(String fileName) {
+        soundEffect = new AudioClip(getClass().getResource("/sound/" + fileName).toExternalForm());
+    }
+
+    public void playClip() {
+        soundEffect.play();
+    }
 }
+
+
+
