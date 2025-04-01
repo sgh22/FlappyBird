896af2d26481f476a40f1ab1bfa5b90963934eea
Brynja <bkb17@hi.is>
2025-04-01 00:07:29 +0000

    handler fyrir collision og þá deyr maður

---
+++ src/main/java/hi/flappybird/CollisionHandler.java
@@ -0,0 +1,14 @@
+package hi.flappybird;
+import javafx.scene.shape.Rectangle;
+import java.util.ArrayList;
+
+public class CollisionHandler {
+    public boolean collisionDetection(ArrayList<Rectangle> obstacles , Rectangle bird){
+        for (Rectangle rectangle: obstacles) {
+            if(rectangle.getBoundsInParent().intersects(bird.getBoundsInParent())){
+                return true;
+            }
+        }
+        return  false;
+    }
+}
