32c85828b57dec5b691258b9fa8926bed65cf1ad
sgh22 <sgh22@hi.is>
2025-04-04 09:57:08 +0000

    bætti við javadoc

--- src/main/java/hi/flappybird/CollisionHandler.java
+++ src/main/java/hi/flappybird/CollisionHandler.java
@@ -3,6 +3,13 @@ import javafx.scene.shape.Rectangle;
 import java.util.ArrayList;
 
 public class CollisionHandler {
+    /**
+     * Athugar hvort fuglinn (rétthyrningurinn) rekist á einhverja af hindrununum.
+     *
+     * @param obstacles Listi af öllum hindrunum sem fuglinn þarf að forðast
+     * @param bird      Rétthyrningurinn sem táknar fuglinn í leiknum
+     * @return true ef fuglinn rekst á hindrun, annars false
+     */
     public boolean collisionDetection(ArrayList<Rectangle> obstacles , Rectangle bird){
         for (Rectangle rectangle: obstacles) {
             if(rectangle.getBoundsInParent().intersects(bird.getBoundsInParent())){
