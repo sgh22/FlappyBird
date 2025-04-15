29b7e7653979a836431932b854839ac4dea7bed0
sgh22 <sgh22@hi.is>
2025-04-15 22:15:55 +0000

    bæta við myndum fyrir bleika fuglinn

--- src/main/java/hi/flappybird/vinnsla/PinkBird.java
+++ src/main/java/hi/flappybird/vinnsla/PinkBird.java
@@ -1,13 +1,11 @@
 package hi.flappybird.vinnsla;
 
-import javafx.scene.image.Image;
-
 public class PinkBird extends Bird {
     public PinkBird() {
         super(new String[]{
-                //"/images/pinkbird1.png",
-                //"/images/pinkbird2.png",
-                //"/images/pinkbird3.png"
+                "/images/pinkbird1.png",
+                "/images/pinkbird2.png",
+                "/images/pinkbird3.png"
         });
     }
 }
