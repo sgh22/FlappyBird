4fe1db5c1edaa97c89d58f6ba33d785500ab8e08
sgh22 <sgh22@hi.is>
2025-04-15 23:40:26 +0000

    gera birds css

--- src/main/java/hi/flappybird/vidmot/BirdSelectionController.java
+++ src/main/java/hi/flappybird/vidmot/BirdSelectionController.java
@@ -3,7 +3,6 @@ package hi.flappybird.vidmot;
 import hi.flappybird.vinnsla.SelectedBird;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -16,40 +15,76 @@ import java.io.IOException;
 public class BirdSelectionController {
 
     @FXML
+    private ImageView pinkBirdImage;
+
+    @FXML
+    private ImageView blueBirdImage;
+
+    @FXML
+    private ImageView dollyBirdImage;
+
+    @FXML
     private ImageView previewImage;
 
+    private String selectedBird;
+
     @FXML
-    private void choosePinkBird(ActionEvent event) throws IOException {
-        SelectedBird.setSelectedBird("pink");
+    public void initialize() {
+        pinkBirdImage.setImage(new Image(getClass().getResourceAsStream("/images/pinkbird1.png")));
+        blueBirdImage.setImage(new Image(getClass().getResourceAsStream("/images/bluebird1.png")));
+        dollyBirdImage.setImage(new Image(getClass().getResourceAsStream("/images/DollyBird1.png")));
+    }
+
+
+    private void clearSelectionStyles() {
+        pinkBirdImage.getStyleClass().remove("selected-bird");
+        blueBirdImage.getStyleClass().remove("selected-bird");
+        dollyBirdImage.getStyleClass().remove("selected-bird");
+    }
+
+
+    @FXML
+    private void previewPinkBird() {
+        clearSelectionStyles();
+        pinkBirdImage.getStyleClass().add("selected-bird");
         updatePreview("/images/pinkbird1.png");
-        goBackToMainMenu(event);
+        selectedBird = "pink";
     }
 
+
     @FXML
-    private void chooseBlueBird(ActionEvent event) throws IOException {
-        SelectedBird.setSelectedBird("blue");
+    private void previewBlueBird() {
+        clearSelectionStyles();
+        blueBirdImage.getStyleClass().add("selected-bird");
         updatePreview("/images/bluebird1.png");
-        goBackToMainMenu(event);
+        selectedBird = "blue";
     }
 
     @FXML
-    private void chooseDollyBird(ActionEvent event) throws IOException {
-        SelectedBird.setSelectedBird("dolly");
+    private void previewDollyBird() {
+        clearSelectionStyles();
+        dollyBirdImage.getStyleClass().add("selected-bird");
         updatePreview("/images/DollyBird1.png");
-        goBackToMainMenu(event);
+        selectedBird = "dolly";
     }
+
+
 
     private void updatePreview(String imagePath) {
         Image birdImage = new Image(getClass().getResourceAsStream(imagePath));
         previewImage.setImage(birdImage);
     }
-
-    private void goBackToMainMenu(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/hi/flappybird/main-menu.fxml"));
-        Scene scene = new Scene(loader.load(), 400, 600);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+    @FXML
+    private void confirmSelection(ActionEvent event) throws IOException {
+        if (selectedBird != null) {
+            SelectedBird.setSelectedBird(selectedBird);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/hi/flappybird/main-menu.fxml"));
+            Scene scene = new Scene(loader.load(), 400, 600);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        }
     }
+
 }
 
