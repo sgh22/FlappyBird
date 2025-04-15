f53401ff9586fdc86d20febfa4b302b8107197ea
sgh22 <sgh22@hi.is>
2025-04-15 22:07:28 +0000

    setti images inn fyrir blue bird

--- src/main/java/hi/flappybird/vinnsla/BlueBird.java
+++ src/main/java/hi/flappybird/vinnsla/BlueBird.java
@@ -1,13 +1,11 @@
 package hi.flappybird.vinnsla;
 
-import javafx.scene.image.Image;
-
 public class BlueBird extends Bird {
     public BlueBird() {
         super(new String[]{
-                //"/images/bluebird1.png",
-                //"/images/bluebird2.png",
-                //"/images/bluebird3.png"
+                "/images/bluebird1.png",
+                "/images/bluebird2.png",
+                "/images/bluebird3.png"
         });
     }
 }
