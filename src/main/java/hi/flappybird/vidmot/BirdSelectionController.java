71a84fc235f7b8415c56e0bfa2a4e38c5389509f
sgh22 <sgh22@hi.is>
2025-04-15 23:04:41 +0000

    laga css

--- src/main/java/hi/flappybird/vidmot/BirdSelectionController.java
+++ src/main/java/hi/flappybird/vidmot/BirdSelectionController.java
@@ -1,33 +1,47 @@
 package hi.flappybird.vidmot;
 
+import hi.flappybird.vinnsla.SelectedBird;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import hi.flappybird.vinnsla.SelectedBird;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Node;
 
 import java.io.IOException;
 
 public class BirdSelectionController {
 
     @FXML
+    private ImageView previewImage;
+
+    @FXML
     private void choosePinkBird(ActionEvent event) throws IOException {
         SelectedBird.setSelectedBird("pink");
+        updatePreview("/images/pinkbird1.png");
         goBackToMainMenu(event);
     }
 
     @FXML
     private void chooseBlueBird(ActionEvent event) throws IOException {
         SelectedBird.setSelectedBird("blue");
+        updatePreview("/images/bluebird1.png");
         goBackToMainMenu(event);
     }
 
     @FXML
     private void chooseDollyBird(ActionEvent event) throws IOException {
         SelectedBird.setSelectedBird("dolly");
+        updatePreview("/images/DollyBird1.png");
         goBackToMainMenu(event);
+    }
+
+    private void updatePreview(String imagePath) {
+        Image birdImage = new Image(getClass().getResourceAsStream(imagePath));
+        previewImage.setImage(birdImage);
     }
 
     private void goBackToMainMenu(ActionEvent event) throws IOException {
@@ -38,3 +52,4 @@ public class BirdSelectionController {
         stage.show();
     }
 }
+
