5fd2fdac79fe28e533bd58933550a94487fce7cd
sgh22 <sgh22@hi.is>
2025-04-15 22:17:43 +0000

    tengja þema og takka og vista valið

--- src/main/java/hi/flappybird/vinnsla/SelectedTheme.java
+++ src/main/java/hi/flappybird/vinnsla/SelectedTheme.java
@@ -1,13 +1,15 @@
 package hi.flappybird.vinnsla;
 
 public class SelectedTheme {
-    private static String selectedTheme = "blue"; // default theme
+    private static String selectedTheme = "pink";
 
     public static void setTheme(String theme) {
+
         selectedTheme = theme;
     }
 
     public static String getTheme() {
+
         return selectedTheme;
     }
 }
