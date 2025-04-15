bffa3636976fb3bd5c38b31eb02f9a77e7b3dc73
sgh22 <sgh22@hi.is>
2025-04-15 22:07:54 +0000

    setti images inn fyrir Dolly bird

--- src/main/java/hi/flappybird/vinnsla/DollyBird.java
+++ src/main/java/hi/flappybird/vinnsla/DollyBird.java
@@ -1,13 +1,11 @@
 package hi.flappybird.vinnsla;
 
-import javafx.scene.image.Image;
-
 public class DollyBird extends Bird {
     public DollyBird() {
         super(new String[]{
-                //"/images/dollybird1.png",
-                //"/images/dollybird2.png",
-                //"/images/dollybird3.png"
+                "/images/DollyBird1.png",
+                "/images/DollyBird2.png",
+                "/images/DollyBird3.png"
         });
     }
 }
